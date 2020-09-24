b7708f5ff6ac2c4cc09637e5cd60673a1ff46d87
sengulali <aslandamla46@gmail.com>
2020-09-24 22:30:21 +0200

    day30 ders sonu

---
+++ target/default-cucumber-reports/report.js
@@ -0,0 +1,229 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/amazonsearch.feature");
+formatter.feature({
+  "name": "Amazon Arama",
+  "description": "",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "name": "@urunarama"
+    }
+  ]
+});
+formatter.background({
+  "name": "Oncesinde Caliscak Method (BeforeMethod)",
+  "description": "",
+  "keyword": "Background"
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici amazon sayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_amazon_sayfasina_gider()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC02_kullanici amazonda arama yapar",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@urunarama"
+    },
+    {
+      "name": "@amazon"
+    }
+  ]
+});
+formatter.step({
+  "name": "kullanici aramakutusuna headphones yazar ve arar",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_aramakutusuna_headphones_yazar_ve_arar()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici sonuc sayisini ekrana yazdirir",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_sonuc_sayisini_ekrana_yazdirir()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});   {
+      "name": "@birdenfazlaexample"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici \"http://amazon.com\" sayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.EbayStepDefinitions.kullanici_sayfasina_gider(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici dropdownda \"Automotive\" secer",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_dropdownda_secer(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici aramakutusuna \"phone holder\" yazar ve arar",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_aramakutusuna_yazar_ve_arar(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici sonuc sayisini ekrana yazdirir",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_sonuc_sayisini_ekrana_yazdirir()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC07_kullanici amazonda dropdown ve aramakutusu kullanarak arama yapar",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@birdenfazlaexample"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici \"http://amazon.com\" sayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.EbayStepDefinitions.kullanici_sayfasina_gider(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici dropdownda \"Baby\" secer",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_dropdownda_secer(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici aramakutusuna \"stroller\" yazar ve arar",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_aramakutusuna_yazar_ve_arar(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici sonuc sayisini ekrana yazdirir",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_sonuc_sayisini_ekrana_yazdirir()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "TC07_kullanici amazonda dropdown ve aramakutusu kullanarak arama yapar",
+  "description": "",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "name": "@birdenfazlaexample"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici \"http://amazon.com\" sayfasina gider",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "stepdefinitions.EbayStepDefinitions.kullanici_sayfasina_gider(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici dropdownda \"Books\" secer",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_dropdownda_secer(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici aramakutusuna \"Miserables\" yazar ve arar",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_aramakutusuna_yazar_ve_arar(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "kullanici sonuc sayisini ekrana yazdirir",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "stepdefinitions.AmazonStepDefinition.kullanici_sonuc_sayisini_ekrana_yazdirir()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
